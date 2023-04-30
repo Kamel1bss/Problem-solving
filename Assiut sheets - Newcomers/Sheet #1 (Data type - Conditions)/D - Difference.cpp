bca35300e6b4f8563501d31548045966093a60e1
Ahmed <67634727+Kamel1bss@users.noreply.github.com>
2023-04-30 07:12:22 +0200

    Add files via upload

---
+++ Assiut sheets - Newcomers/Sheet #1 (Data type - Conditions)/D - Difference.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    long long  a, b, c, d;
+    long long sum;
+    cin >> a >> b >> c >> d;
+    sum = (a * b) - (c * d);
+    cout << "Difference = " << sum;
+
+    return 0;
+}
