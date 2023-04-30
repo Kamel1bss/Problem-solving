98b4369834ff5e1fa3d71ca777fe5aec949d1306
Ahmed <67634727+Kamel1bss@users.noreply.github.com>
2023-04-30 07:18:15 +0200

    Add files via upload

---
+++ Assiut sheets - Newcomers/Sheet #1 (Data type - Conditions)/F - Digits Summation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    long long a, b;
+    int sum;
+    cin >> a >> b;
+    a %= 10;
+    b %= 10;
+    sum = a + b;
+    cout << sum;
+    
+    return 0;
+}
