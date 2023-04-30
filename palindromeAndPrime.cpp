9db5b588d32ce07201926179cd2dc983ecd415d5
Ahmed <67634727+Kamel1bss@users.noreply.github.com>
2023-04-30 06:34:55 +0200

    Add files via upload

---
+++ palindromeAndPrime.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// function checks palindrome
+bool is_palindrome(int num)
+{
+    bool palindrome = true;
+    string str = "";
+    while(num)
+    {
+        int digit = num % 10;
+        num /= 10;
+        char c =  digit + '0';
+        str += c;
+    }
+
+    for(int i = 0; i < str.size() / 2; i++)
+    {
+        if (str[i] != str[str.size() - 1 - i])
+        {
+            palindrome = false;
+            break;
+        }
+    }
+
+    return palindrome;
+}
+
+// functions checks prime
+bool is_prime(int num)
+{
+    if (num == 0 || num == 1)
+        return false;
+
+    bool prime = true;
+    for(int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+
+    return prime;
+}
+
+int main() {
+
+    int num;    cin >> num;
+    for(int i = 2 ; i < num; i++)
+    {
+        if (is_palindrome(i) && is_prime(i))
+            cout  << i << endl;
+    }
+    return 0;
+}
