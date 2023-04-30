b7b3d4efe5ffb17435763a4cafac808e57e18516
Ahmed <67634727+Kamel1bss@users.noreply.github.com>
2023-04-30 08:08:19 +0200

    Add files via upload

---
+++ Assiut sheets - Newcomers/Sheet #1 (Data type - Conditions)/V - Comparison.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+    char c;
+    cin >> a >> c >> b;
+
+    switch(c)
+    {
+        case '<':
+           if(a < b)
+		cout<<"Right";
+           else
+		cout <<"Wrong";
+        break;
+        case '>':
+           if(a > b)
+		cout<<"Right";
+           else
+		cout <<"Wrong";
+        break;
+        case '=':
+           if(a == b)
+		cout<<"Right";
+           else
+		cout <<"Wrong";
+        break;
+    }
+
+}
+
+
